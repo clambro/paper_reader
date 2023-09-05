882a2b4508349b47362ec814a5be5759fa34789d
Costa <clambro.git@gmail.com>
2023-09-04 19:39:24 -0700

    Refactored variable names

--- src/get_paper_references.py
+++ src/get_paper_references.py
@@ -32,23 +32,22 @@ def main(paperdigest_url, output_folder):
 
     logging.info(f'Pulling the HTML string from the response.')
     all_paper_html = html.fromstring(response.content)
-    rows = all_paper_html.xpath('//div[@class="blog-post__content"]/table/tr/td[2]')
+    paper_rows = all_paper_html.xpath('//div[@class="blog-post__content"]/table/tr/td[2]')
 
-    logging.info(f'Detected {len(rows)} papers. Pulling details.')
+    logging.info(f'Detected {len(paper_rows)} papers. Pulling details.')
     output = []
-    for r in tqdm(rows):
-        title = r.xpath('a/b/text()')[0]
-        paperdigest_url = r.xpath('a/@href')[0]
+    for row in tqdm(paper_rows):
+        title = row.xpath('a/b/text()')[0]
+        paperdigest_url = row.xpath('a/@href')[0]
         url_slug = re.search('forum-id-(.+?)-', paperdigest_url)[1]
         url = 'https://openreview.net/forum?id=' + url_slug
-        blurb = r.xpath('small/i/text()')[0][2:]  # Text starts with ": "
-        output.append((title, url, blurb))
+        highlight = row.xpath('small/i/text()')[0][2:]  # Text starts with ": "
+        output.append((title, url, highlight))
 
-    df_orig = pd.DataFrame(output, columns=['title', 'url', 'highlight'])
-    df_orig = df_orig.sample(frac=1, replace=False, random_state=2112).reset_index(drop=True)  # Shuffle.
+    df = pd.DataFrame(output, columns=['title', 'url', 'highlight'])
 
     output_path = os.path.join(output_folder, 'raw_paperdigest_data.csv')
-    df_orig.to_csv(output_path, index=False)
+    df.to_csv(output_path, index=False)
 
 
 if __name__ == '__main__':
